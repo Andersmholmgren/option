3b3c0912cd34893cc40cc1551087ceef4cfe0fce
Joseph Moniz <joseph.moniz@gmail.com>
2013-04-17 18:11:12 -0700

    version bumping

--- test/option_test.dart
+++ test/option_test.dart
@@ -2,16 +2,19 @@ import "package:unittest/unittest.dart";
 import "package:option/option.dart";
 
 main () {
-  
-  test("create Some by Option factory constructor", () {
+
+  test("Create Some by Option factory constructor", () {
     expect(new Option(20) is Some, isTrue);
     expect(new Option(20), isNot(same(new Option(20))),
-        reason: "Some is not constant if it is created by Option factory constructor.");
+        reason: "Some is not constant when created by Option factory ctor."
+    );
   });
-  
-  test("create None by Option factory costructor", () {
-    expect(new Option(null) is None, isTrue);
-    expect(new Option(null), same(new Option(null)), reason: "None is constant.");
-  });
-}
 
+  test("Create None by Option factory costructor", () {
+    expect(new Option(null) is None, isTrue);
+    expect(new Option(null), same(new Option(null)),
+        reason: "None is constant."
+    );
+  });
+
+}
